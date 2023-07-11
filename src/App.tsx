832a3a7d592d7125392902916f46e1b522fd8df9
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-11 21:41:49 +0200

    render list of news titles.

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,44 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
 
 function App() {
+  const [news, setNews] = useState([])
 
-
-function handleApiCall(){
-  fetch("https://www.tagesschau.de/api2/homepage")
-    .then(res => {
-      console.table(res)
+  function handleApiCall(){
+    fetch("https://www.tagesschau.de/api2/news/?regions=1&ressort=ausland", {
+      method: 'GET',
+      headers: {
+          'Accept': 'application/json',
+      }
     })
+    .then(response => response.json())
+      .then(data => {
+        setNews(data.news)
+        console.log(data.news)
+      })
 
 
-}
+  }
 
   return (
     <>
       <Button variant="contained" onClick={handleApiCall}>Get news</Button>
+
+      <ul>
+      {
+        news.map((item: any, index: number) => {
+          return <li key={index}>{item.title}</li>
+        })
+      }
+      </ul>
+      <Card/>
+     
+
     </>
   )
 }
+
 
 export default App
