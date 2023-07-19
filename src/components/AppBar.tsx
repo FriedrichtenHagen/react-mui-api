8a2881e41fb6a1009d8d6dcc497bc45e77318fb0
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-07-19 19:26:27 +0200

    Remove icon in header.

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -94,7 +94,7 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+
           <Typography
             variant="h5"
             noWrap
